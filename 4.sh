74c18a08c37e13313ca0881aa2dac639a89383f7
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 01:35:59 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -117,4 +117,5 @@ docker run -d --name=wxedge name=wxed -e LISTEN_ADDR=":38888" --restart=always -
 
 Server_IP=`curl ifconfig.me`
 echo $Server_IP":28888" >> file.txt
+echo $Server_IP":38888" >> file.txt
 expect 5.sh
