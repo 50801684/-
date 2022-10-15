4402709aef926b62db53a3ed06a696884a731615
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 00:12:06 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -3,10 +3,3 @@ wget https://raw.fastgit.org/50801684/-/master/4.sh
 wget https://raw.fastgit.org/50801684/-/master/5.sh
 wget https://raw.fastgit.org/50801684/-/master/file.txt
 wget https://raw.fastgit.org/50801684/-/master/7.sh
-sh 4.sh
-sh 5.sh
-sh 6.sh
-sh 7.sh
-
-./4.sh -d 
-echo "安装完成"
