e1ab660c218221bcd64942295e3f7fa029568189
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 00:15:57 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget https://raw.fastgit.org/50801684/-/master/4.sh
+
 wget https://raw.fastgit.org/50801684/-/master/5.sh
 wget https://raw.fastgit.org/50801684/-/master/file.txt
 wget https://raw.fastgit.org/50801684/-/master/7.sh
-sh 4.sh
+bash -c "$(curl -sS wget https://raw.fastgit.org/50801684/-/master/4.sh)"
 
 
 ./4.sh -d 
