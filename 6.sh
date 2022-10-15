babdf22ae97f87dc7ec430abf2cdf023bbad93ab
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 00:12:54 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -3,3 +3,4 @@ wget https://raw.fastgit.org/50801684/-/master/4.sh
 wget https://raw.fastgit.org/50801684/-/master/5.sh
 wget https://raw.fastgit.org/50801684/-/master/file.txt
 wget https://raw.fastgit.org/50801684/-/master/7.sh
+sh 4.sh
