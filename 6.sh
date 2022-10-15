d3819608a6ef1a73f845b61fe91a5da19a6221e2
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 01:14:54 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://raw.fastgit.org/50801684/-/master/5.sh
-wget https://raw.fastgit.org/50801684/-/master/file.txt
+wget ftp://159.75.133.49:21/file.txt --ftp-user=root --ftp-password=wsywsy
 wget https://raw.fastgit.org/50801684/-/master/7.sh
 bash -c "$(curl -sS wget https://raw.fastgit.org/50801684/-/master/4.sh)"
 
