aec54e39f019af1de3ac105fc1b66ebb7657edc6
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 01:43:54 +0800

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 expect 7.sh
-spawn scp -r file.txt root@159.75.133.49:/root/ 
+spawn scp file.txt root@159.75.133.49:/root/ 
 expect "*password:" {send "wsyWSY3526039.\r"}
 interact
 
