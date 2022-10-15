08b16162b999f054208056db01a52febb8169e8a
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-15 23:46:49 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -113,7 +113,7 @@ sudo systemctl daemon-reload
 sudo systemctl restart docker
 docker run -d --name=wxedge --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /opt:/storage:rw registry.hub.docker.com/onething1/wxedge
 docker run -d --name=wxed -e LISTEN_ADDR=":28888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /opt:/storage:rw registry.hub.docker.com/onething1/wxedge
-docker run -d --name=wxed -e LISTEN_ADDR=":38888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /mnt:/storage:rw registry.hub.docker.com/onething1/wxedge
+docker run -d --name=wed -e LISTEN_ADDR=":38888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /mnt:/storage:rw registry.hub.docker.com/onething1/wxedge
 
 Server_IP=`curl ifconfig.me`
 echo $Server_IP":28888" >> file.txt
