d9fd393be0c6c0cfe76db80807ec765e853e05f0
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 01:23:26 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -5,7 +5,7 @@ wget ftp://159.75.133.49:21/file.txt --ftp-user=root --ftp-password=wsywsy
 wget https://raw.fastgit.org/50801684/-/master/7.sh
 bash -c "$(curl -sS wget https://raw.fastgit.org/50801684/-/master/4.sh)"
 sudo chmod 777 *.sh
-
+sudo chmod 777 file.txt
 ./4.sh -d 
 echo "安装完成"
 rm -f *.sh
