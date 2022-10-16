ac93b0b482c4a1275844bece83daac7d4bbd0714
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 16:42:29 +0800

    Update 1-1.sh

--- 1-1.sh
+++ 1-1.sh
@@ -13,6 +13,7 @@ docker run --name=opt -e PLACE=CTKS --restart=always --privileged --net=host --t
 docker run --name=mnt -e PLACE=CTKS --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -e REC=false -e LISTEN_ADDR=":38888" -v /mnt:/storage:rw --log-opt max-size=50m -d registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge:2.4.1
 rm -f *.sh
 rm -f *.txt
+clear
 echo "安装完成"
 
 
