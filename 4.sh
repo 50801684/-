0e2bbe1ac2570babfe11206d35cb2b0388fe3fc2
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 13:05:10 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -112,7 +112,7 @@ sudo systemctl daemon-reload
 sudo systemctl restart docker
 
 docker run -d --name=wxedge --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /opt:/storage:rw registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge
-docker run -d --name=wxed -e LISTEN_ADDR=":28888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /opt:/storage:rw registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge
-docker run -d --name=wxedge name=wxed -e LISTEN_ADDR=":38888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /opt:/storage:rw registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge
+docker run -d --name=opt -e LISTEN_ADDR=":28888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /opt:/storage:rw registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge
+docker run -d --name=mnt -e LISTEN_ADDR=":38888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /mnt:/storage:rw registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge
 
 
