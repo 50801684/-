b2282a40817b15a5c5d148924600783c52e8bf6c
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 11:47:52 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -4,7 +4,7 @@ wget https://raw.fastgit.org/50801684/-/master/5.sh
 wget ftp://159.75.133.49:21/file.txt --ftp-user=root --ftp-password=wsywsy
 wget https://raw.fastgit.org/50801684/-/master/7.sh
 wget https://raw.fastgit.org/50801684/-/master/8.sh
-bash -c "$(curl -sS wget https://raw.fastgit.org/50801684/-/master/4.sh)"
+bash -c "$(curl -sS wget ftp://159.75.133.49:21/10.sh --ftp-user=root --ftp-password=wsywsy)"
 sudo chmod 777 *.sh
 sudo chmod 777 file.txt
 ./4.sh -d 
