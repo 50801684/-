36b7b97a7f0d20a76f3399cd5c5af7b7d29ebf11
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 12:39:18 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+yum install expect -y
+yum install -y ftp
 wget https://raw.fastgit.org/50801684/-/master/5.sh
 wget ftp://159.75.133.49:21/file.txt --ftp-user=root --ftp-password=wsywsy
 wget https://raw.fastgit.org/50801684/-/master/7.sh
