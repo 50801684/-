87043457c6b7c1f2c118f17d882d70b2561f3d3d
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 16:31:43 +0800

    Update 1-1.sh

--- 1-1.sh
+++ 1-1.sh
@@ -1,7 +1,4 @@
-Server_IP=`curl ifconfig.me`
-echo $Server_IP":28888" >> file.txt
-echo $Server_IP":38888" >> file.txt
-expect 5.sh
+
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
@@ -11,5 +8,11 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-docker run -d --name=wxed -e LISTEN_ADDR=":28888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /opt:/storage:rw registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge
-docker run -d --name=wxedge name=wxed -e LISTEN_ADDR=":38888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /opt:/storage:rw registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge
+docker run --name=wxedge -e PLACE=CTKS --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -e REC=false -e LISTEN_ADDR=":18888" -v /opt:/storage:rw --log-opt max-size=50m -d registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge:2.4.1
+docker run --name=opt -e PLACE=CTKS --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -e REC=false -e LISTEN_ADDR=":28888" -v /opt/1:/storage:rw --log-opt max-size=50m -d registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge:2.4.1
+docker run --name=mnt -e PLACE=CTKS --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -e REC=false -e LISTEN_ADDR=":38888" -v /mnt:/storage:rw --log-opt max-size=50m -d registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge:2.4.1
+rm -f *.sh
+rm -f *.txt
+echo "安装完成"
+
+
