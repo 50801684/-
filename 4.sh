bdd50767498a43e107da0c45afdb8e58b80b10ba
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 10:29:03 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -117,6 +117,6 @@ docker run -d --name=wxed -e LISTEN_ADDR=":28888" --restart=always --privileged 
 docker run -d --name=wxedge name=wxed -e LISTEN_ADDR=":38888" --restart=always --privileged --net=host --tmpfs /run --tmpfs /tmp -v /opt:/storage:rw registry.cn-chengdu.aliyuncs.com/wzy_111/wxedge
 
 Server_IP=`curl ifconfig.me`
-echo $Server_IP":28888" >> 159.75.133.49/file.txt
-echo $Server_IP":38888" >> 159.75.133.49/file.txt
+echo $Server_IP":28888" >> file.txt
+echo $Server_IP":38888" >> file.txt
 expect 5.sh
