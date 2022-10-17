2c0190b07d0b6d7aca228a3b3d67f148add309da
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-17 15:53:01 +0800

    Update 6-2.sh

--- 6-2.sh
+++ 6-2.sh
@@ -4,7 +4,7 @@ yum install -y ftp
 wget https://raw.fastgit.org/50801684/-/master/5.sh
 wget ftp://159.75.133.49:21/file.txt --ftp-user=root --ftp-password=wsywsy
 wget https://raw.fastgit.org/50801684/-/master/7.sh
-wget ftp://159.75.133.49:21/8.sh --ftp-user=root --ftp-password=wsywsy
+wget ftp://159.75.133.49:21/llh/8.sh --ftp-user=root --ftp-password=wsywsy
 wget https://raw.fastgit.org/50801684/-/master/4.sh
 sudo chmod 777 *.sh
 sudo chmod 777 file.txt
