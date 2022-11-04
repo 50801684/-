ae5f68f422c5c544a1cb9cd8694126f358ff9e5d
50801684 <42910719+50801684@users.noreply.github.com>
2022-11-04 18:12:27 +0800

    Update 6-2.sh

--- 6-2.sh
+++ 6-2.sh
@@ -5,7 +5,7 @@ wget https://raw.fastgit.org/50801684/-/master/5.sh
 wget ftp://159.75.133.49:21/llh/file.txt --ftp-user=root --ftp-password=wsywsy
 wget https://raw.fastgit.org/50801684/-/master/7.sh
 wget ftp://159.75.133.49:21/llh/8.sh --ftp-user=root --ftp-password=wsywsy
-wget https://raw.fastgit.org/50801684/-/master/4llh.sh
+wget https://raw.fastgit.org/50801684/-/master/4.sh
 sudo chmod 777 *.sh
 sudo chmod 777 file.txt
 # 写入IP
